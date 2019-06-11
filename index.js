94810df9047eb86f3a04a10fad0944685d8d2690
rodgerss <mwangekarodgers@gmail.com>
2019-06-11 13:57:58 +0300

    installing nodemon installing dotenv configuring dotenv

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require ("mysql");
 const app = express();
 
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "foo",
-    password: "PASSWORD",
-    database: "reecipe_box"
+   host: process.env.DB_HOST,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASS,
+   database: process.env.DB_NAME
 });
 
 app.get("/", (req, res) => res.send("Hello World!"));
