75cda013d60d19e42b200d29c209453652f78ce4
rodgerss <mwangekarodgers@gmail.com>
2019-07-09 09:53:10 +0300

    GET,POST ,PUT, DELETE

--- index.js
+++ index.js
@@ -41,7 +41,37 @@ app.get("/api/recipe/home",(reg, res) => {
         }
     );
 });
+app.get("/api/recipe/home:id", (req, res) => {
+    pool.query(
+        "SELECT recipe_id,recipe_name, image FROM recipe WHERE id = ?",
+        [req.params.id],
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
 
+            res.json(rows);
+        }
+    );
+});
+
+app.get("/api/recipe/home:id/name", (req, res) => {
+    pool.query(
+        `SELECT r.recipe_name, r.recipe_image,u.name
+        FROM recipe r
+        JOIN user u ON u.recipe_id = r.id 
+        WHERE r.id = ?
+        ORDER BY r.recipe_name`,
+        [req.params.id],
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            res.json(rows);
+        }
+    );
+});
 app.get("/api/recipe/add",(reg, res) => {
     pool.query(
         "SELECT r.recipe_name,r.ingredients,r.method,r.image,u.name FROM recipe as r join user as u;",
@@ -49,6 +79,39 @@ app.get("/api/recipe/add",(reg, res) => {
             if (error){
                 return res.status(500).json({error});
 
+            }
+
+            res.json(rows);
+        }
+    );
+});
+
+app.get("/api/recipe/add:id", (req, res) => {
+    pool.query(
+        "SELECT id, name, ingredients,method WHERE id = ?",
+        [req.params.id],
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            res.json(rows);
+        }
+    );
+});
+
+app.get("/api/recipe/add:id/name", (req, res) => {
+    pool.query(
+        `SELECT r.recipe_name,r.ingredients,r.method,r.image,u.name
+        FROM recipe r
+        JOIN user u ON u recipe_id = r.id
+        WHERE r.recipe_id = ?
+        GROUP BY r.recipe_id 
+        ORDER BY r.recipe_name,u.name `,
+        [req.params.id],
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
             }
 
             res.json(rows);
@@ -69,16 +132,29 @@ app.get("/api/recipe/show",(reg,res) => {
         }
     );
 });
-app.post("/reecipe_box", (req, res) => {
-    const recipe = req.body;
+app.get("/api/recipe/show:id", (req, res) => {
+    pool.query(
+        "SELECT image,ingredients,method FROM recipe WHERE id = ?",
+        [req.params.id],
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
 
-    if (!recipe.name) {
+            res.json(rows);
+        }
+    );
+});
+app.post("api/user", (req, res) => {
+    const user = req.body;
+
+    if (!user.name) {
         return res.status(400).json({ error: "Invalid payload" });
     }
 
     pool.query(
-        "INSERT INTO recipe (name) VALUES (?)",
-        [recipe.name],
+        "INSERT INTO user (name) VALUES (?)",
+        [user.name],
         (error, results) => {
             if (error) {
                 return res.status(500).json({ error });
@@ -89,6 +165,92 @@ app.post("/reecipe_box", (req, res) => {
     );
 });
 
-   
+app.put("api/user/:id", (req, res) => {
+    const user = req.body;
+
+    if (!user.name) {
+        return res.status(400).json({ error: "Invalid payload" });
+    }
+
+    pool.query(
+        "UPDATE user SET name = ? WHERE id = ?",
+        [user.name, req.params.id],
+        (error, results) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            res.json(results.changedRows);
+        }
+    );
+});
+app.delete("api/user/:id", (req, res) => {
+    pool.query(
+        "DELETE FROM recipe WHERE id = ?",
+        [req.params.id],
+        (error, results) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            res.json(results.affectedRows);
+        }
+    );
+});
+
+
+app.post("/api/recipe", (req, res) => {
+    const recipe = req.body;
+
+    if (!recipe.name || !recipe.ingredients || !recipe.method || !recipe.image) {
+        return res.status(400).json({ error: "Invalid payload" });
+    }
+
+    pool.query(
+        "INSERT INTO recipe (name, ingredients, method,  image) VALUES (?, ?, ?, ?)",
+        [recipe.name, recipe.ingredients, recipe.method, recipe.image ],
+        (error, results) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            res.json(results.insertId);
+        }
+    );
+});
+
+app.put("/api/recipe/:id", (req, res) => {
+    const recipe = req.body;
+
+    if (!recipe.name || !recipe.ingredients ||!recipe.method || !recipe.image) {
+        return res.status(400).json({ error: "Invalid payload" });
+    }
+                   pool.query(
+        "UPDATE recipe SET name = ?, ingredients = ?, method = ?, image = ? WHERE id = ?",
+        [recipe.name, recipe.ingredients, recipe.method, recipe.image, req.params.id],
+        (error, results) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            res.json(results.changedRows);
+        }
+   );
+});
+
+app.delete("/api/recipe/:id", (req, res) => {
+    pool.query(
+        "DELETE FROM recipe WHERE id = ?",
+        [req.params.id],
+        (error, results) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+
+            res.json(results.affectedRows);
+        }
+    );
+});
+
 app.listen(9000, () => console.log("App listening on port 9000"));
 
