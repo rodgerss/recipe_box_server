1bea631e01b03b53cf899c85a87bded73c869c3c
rodgerss <mwangekarodgers@gmail.com>
2019-07-30 10:37:59 +0300

    add loading

---
+++ client/src/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Loading() {
+    return (
+        <div className="rcls-fullpage-text">
+            <p>Loading...</p>
+        </div>
+
+    );
+
+}
+
+export default Loading;
