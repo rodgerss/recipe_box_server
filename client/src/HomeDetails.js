56409163622834f6d0bac02143c837be9632d6e6
rodgerss <mwangekarodgers@gmail.com>
2019-07-30 11:42:43 +0300

    added homedetails

---
+++ client/src/HomeDetails.js
@@ -0,0 +1,10 @@
+import  React from "react";
+
+function HomeDetails(props) {
+    return (
+        <div>
+            <h1>Home {props.recipe_id} Details</h1>
+        </div>
+    );
+}
+export default HomeDetails;
