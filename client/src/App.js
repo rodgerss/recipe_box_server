f4a746d7a4d95d6a1bab0d5fe4580891a90f3665
rodgerss <mwangekarodgers@gmail.com>
2019-07-30 10:20:16 +0300

    made changes for app to start working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router } from "@reach/router"
+import Recipe_box from "./Home"
+import Home from "./Home"
+import Navbar from './Navbar';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="rcls-app">
+            <header className="rcls-header">
+                <Navbar />
+            </header>
+            <main className="rcls-main">
+                 <Router>
+                    <Home path="/recipe/home" />
+                    <homedetails path="/recipe/home:id" />
+                  </Router>
+             
+            </main>
+       </div>
+    );
 }
+
 
 export default App;
