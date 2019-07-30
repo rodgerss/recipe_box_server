414c596bb30b666340eb7f01a11bbf09cb92a407
rodgerss <mwangekarodgers@gmail.com>
2019-07-30 10:43:31 +0300

    added error text working

---
+++ client/src/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Error() {
+    return(
+        <div className="rcls-fullpage-text">
+            <p>Sorry, a server error occured. Please retry.</p>
+        </div>
+
+    );
+
+}
+export default Error;
